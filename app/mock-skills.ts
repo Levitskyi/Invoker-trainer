ba0fe6b88a8204e9175f128c0fb5db090802c1fd
Taras <taraslevitskyi@gmail.com>
2016-11-16 16:34:55 +0200

    added image to skills

--- app/mock-skills.ts
+++ app/mock-skills.ts
@@ -26,60 +26,60 @@ export const SKILLS: Skill[] = [
         id: 1,
         name: 'Cold Snap',
         keys: 'qqq',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_cold_snap_hp1.png'
     },
     {
         id: 2,
         name: 'Ghost Walk',
         keys: 'qqw',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_ghost_walk_hp1.png'
     },
     {
         id: 3,
         name: 'Tornado',
         keys: 'wwq',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_tornado_hp1.png'
     },
     {
         id: 4,
         name: 'EMP',
         keys: 'www',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_emp_hp1.png'
     },
     {
         id: 5,
         name: 'Alacrity',
         keys: 'wwe',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_alacrity_hp1.png'
     },
     {
         id: 6,
         name: 'Chaos Meteor',
         keys: 'eew',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_chaos_meteor_hp1.png'
     },
     {
         id: 7,
         name: 'Sun Strike',
         keys: 'eee',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_sun_strike_hp1.png'
     },
     {
         id: 8,
         name: 'Forge Spirit',
         keys: 'eeq',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_forge_spirit_hp1.png'
     },
     {
         id: 9,
         name: 'Ice Wall',
         keys: 'qqe',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_ice_wall_hp1.png'
     },
     {
         id: 10,
         name: 'Deafening Blast',
         keys: 'qwe',
-        imgUrl: ''
+        imgUrl: 'http://cdn.dota2.com/apps/dota2/images/abilities/invoker_deafening_blast_hp1.png'
     }
 ];
